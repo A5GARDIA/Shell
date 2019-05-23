ccc6f798c2bf57a1c2ece501a5a89057d58b9bd4
Zace <48513797+A5GARDIA@users.noreply.github.com>
2019-05-23 13:48:18 +0200

    Create mail_notification.sh

---
+++ mail_notification.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+free -h > log.txt
+HOSTNAME=$(hostname)
+ USED_MEM=$(grep 'Mem' log.txt | head -c33 | tail -c10)
+
+ if [[ ${?} -ne 0 ]]
+ then
+        echo "  ${USED_MEM}  Critical"
+        exit 1
+else
+        echo " ${HOSTNAME}  USED : ${USED_MEM} of RAM memory" | mail  -s "HOST INFO" email@gmail.com
+fi
